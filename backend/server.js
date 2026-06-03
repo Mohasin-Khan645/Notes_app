645e7eab9a2df1c85ce5d8dff8397b39244f9f3b
Mohammad Mohasin Khan <kmohasin645@gmail.com>
2026-06-03 15:24:18 +0545

    fix: allow dynamic CORS origins for production deployment

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 
 // 2. Middlewares
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
